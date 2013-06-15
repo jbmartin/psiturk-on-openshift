b93c56be4b25e51b98d6716c69cfef28182f4273
Jay B. Martin <jmartin805@gmail.com>
2013-06-15 18:38:34 -0400

    updated database env var bindings

--- psiturk/db.py
+++ psiturk/db.py
@@ -1,11 +1,13 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 from config import config
 
-DATABASE = config.get('Database Parameters', 'database_url')
+# DATABASE = config.get('Database Parameters', 'database_url')
+# DATABASE = 'mysql://' + user + ':' + password + '@' + domain + ':' + port + '/' dbname
+DATABASE = 'mysql://' + os.environ['OPENSHIFT_MYSQL_DB_HOST'] + ':' + os.environ['OPENSHIFT_MYSQL_DB_PORT'] + '/'
 
 engine = create_engine(DATABASE, echo=False) 
 db_session = scoped_session(sessionmaker(autocommit=False,
