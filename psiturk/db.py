fe6699c31d7c5331165b24d956c700739be6e8d9
Jay B. Martin <jmartin805@gmail.com>
2013-06-15 18:46:53 -0400

    create mysql db if it doesn't exist already

--- psiturk/db.py
+++ psiturk/db.py
@@ -5,11 +5,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from config import config
 
+DBNAME = 'psiturk'
 # DATABASE = config.get('Database Parameters', 'database_url')
 # DATABASE = 'mysql://' + user + ':' + password + '@' + domain + ':' + port + '/' dbname
-DATABASE = 'mysql://' + os.environ['OPENSHIFT_MYSQL_DB_HOST'] + ':' + os.environ['OPENSHIFT_MYSQL_DB_PORT'] + '/'
+DATABASE = 'mysql://' + os.environ['OPENSHIFT_MYSQL_DB_USERNAME'] + ':' + os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'] + '@' +
+           os.environ['OPENSHIFT_MYSQL_DB_HOST'] + ':' + os.environ['OPENSHIFT_MYSQL_DB_PORT'] + '/' + DBNAME
 
-engine = create_engine(DATABASE, echo=False) 
+
+engine = create_engine(DATABASE, echo=False)  # connect to db's server
+engine.execute("CREATE DATABASE " + DBNAME)  # create db
+engine.execute("USE " + DBNAME)  # select new db
+
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
