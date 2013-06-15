9d056cc090fb2a1bea9333404d435242ca2322a9
Jay B. Martin <jmartin805@gmail.com>
2013-06-15 17:48:11 -0400

    updated setup config to match master

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+setup(
+    name = "PsiTurkOnOpenShift",
+    version = "0.1",
+    packages = ["psiturk"],
+    entry_points = {
+        'console_scripts': ['psiturk = psiturk.psiturk:run_webserver']
+    },
+    setup_requires = ["Flask", "boto", "SQLAlchemy"],
+    extras_require = {
+        "gunicorn": "gunicorn"
+    },
+    author = "NYU Computation and Cognition Lab",
+    author_email = "http://nyuccl.org",
+    description = "A web framework for dynamic behavioral experiments",
+    url = "http://github.com/NYUCCL/psiturk"
+)
 
-setup(name='PsiTurkOnOpenShift', version='0.1',
-      description="PsiTurk on OpenShift",
-      author='Jay B Martin', author_email='jbmartin@nyu.edu',
-      url='http://www.github.com/nyuccl/psiturk',
-      install_requires=['sqlalchemy', 'Flask', 'boto', 'gunicorn']
-     )
