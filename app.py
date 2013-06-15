e60b6020f08f16769db9b79fc2583e47549421c7
Jay B. Martin <jmartin805@gmail.com>
2013-06-15 17:47:39 -0400

    cleaned up script

--- app.py
+++ app.py
@@ -19,16 +19,11 @@ try:
 except IOError:
    pass
 
-# IMPORTANT: Put any additional includes below this line.  If placed above this
+# IMPORTANT: Put any additional includes below this line. If placed above this
 # line, it's possible required libraries won't be in your searchable path
-#
 
-
-#
-#  main():
-#
 if __name__ == '__main__':
-   ip   = os.environ['OPENSHIFT_PYTHON_IP']
+   ip = os.environ['OPENSHIFT_PYTHON_IP']
    port = 8080
    print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
    try:
