f76dce18ce1687d12036d29e7d0306e79b50b517
Jay B. Martin <jmartin805@gmail.com>
2013-06-15 20:12:36 -0400

    more testing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     entry_points = {
         'console_scripts': ['psiturk = psiturk.psiturk:run_webserver']
     },
-    setup_requires=["Flask", "boto", "SQLAlchemy", "gunicorn", "python-dev", "Flask-SQLAlchemy" 'MySQL-python'],
+    install_requires=["flask", "boto", "sqlalchemy", "gunicorn", "mysql-python"],
     author = "NYU Computation and Cognition Lab",
     author_email = "http://nyuccl.org",
     description = "A web framework for dynamic behavioral experiments",
